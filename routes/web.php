7f8d3bfb177a63a2ed09e1b6e10e07985819fa0c
Silvio Lucas <silviolucas_santos@hotmail.com>
2023-09-19 16:35:47 -0300

    feat: add new routes

--- routes/web.php
+++ routes/web.php
@@ -22,3 +22,13 @@ Route::get('/', [PrincipalController::class, 'principal'])->name('site.index');
 Route::get('/sobre-nos', [SobreNosController::class, 'sobreNos'])->name('site.sobre-nos');
 Route::get('/contato', [ContatoController::class, 'contato'])->name('site.contato');
 Route::get('/login', function () { return 'Login'; })->name('site.login');
+
+Route::prefix('/app')->group(function() {
+    Route::get('/clientes', function () { return 'Clientes'; })->name('app.clientes');
+    Route::get('/fornecedores', function () { return 'Fornecedores'; })->name('app.fornecedores');
+    Route::get('/produtos', function () { return 'Produtos'; })->name('app.produtos');
+});
+
+Route::fallback(function() {
+    echo 'A rota acessada não existe. <a href="'.route('site.index').'">Clique aqui</a> para ir para a página inicial.';
+});
