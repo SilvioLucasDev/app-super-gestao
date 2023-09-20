cf68ad476f58faf0064c9ea7b7a7da13e5c52e2f
Silvio Lucas <silviolucas_santos@hotmail.com>
2023-09-20 15:15:19 -0300

    refactor: apply value old in form

--- app/Http/Controllers/PrincipalController.php
+++ app/Http/Controllers/PrincipalController.php
@@ -8,7 +8,12 @@ class PrincipalController extends Controller
 {
     public function principal()
     {
+        $motivo_contatos = [
+            '1' => 'Dúvida',
+            '2' => 'Elogio',
+            '3' => 'Reclamação',
+        ];
         $titulo = 'Home';
-        return view('site.principal', compact('titulo'));
+        return view('site.principal', compact('titulo', 'motivo_contatos'));
     }
 }
