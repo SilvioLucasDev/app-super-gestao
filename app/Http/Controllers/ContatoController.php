5db7ee58925705e7eb39cba300f55c02a916297e
Silvio Lucas <silviolucas_santos@hotmail.com>
2023-09-20 15:40:42 -0300

    refactor: apply dynamic methods in select

--- app/Http/Controllers/ContatoController.php
+++ app/Http/Controllers/ContatoController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\MotivoContato;
 use Illuminate\Http\Request;
 use App\Models\SiteContato;
 
@@ -9,11 +10,7 @@ class ContatoController extends Controller
 {
     public function contato(Request $request)
     {
-        $motivo_contatos = [
-            '1' => 'Dúvida',
-            '2' => 'Elogio',
-            '3' => 'Reclamação',
-        ];
+        $motivo_contatos = MotivoContato::all();
         $titulo = 'Contato';
         return view('site.contato', compact('titulo', 'motivo_contatos'));
     }
