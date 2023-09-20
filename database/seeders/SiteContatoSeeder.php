7ed78d60cf2d98913fe56d6ae77ec3c40f985806
Silvio Lucas <silviolucas_santos@hotmail.com>
2023-09-20 12:35:17 -0300

    feat: add SiteContatoSeeder

---
+++ database/seeders/SiteContatoSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\SiteContato;
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class SiteContatoSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        SiteContato::factory(100)->create();
+    }
+}
