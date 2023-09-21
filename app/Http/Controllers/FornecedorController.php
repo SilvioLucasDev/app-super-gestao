68c86531706de7b8cecd49e95f70c8797760bcba
Silvio Lucas <silviolucas_santos@hotmail.com>
2023-09-20 23:48:05 -0300

    feat: add FornecedorController and view

---
+++ app/Http/Controllers/FornecedorController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class FornecedorController extends Controller
+{
+    public function index(Request $request)
+    {
+        $titulo = 'Fornecedor';
+        return view('app.fornecedor', compact('titulo'));
+    }
+}
