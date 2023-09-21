8082efd263c1c4f2bf2daf67095c77072e87ca1d
Silvio Lucas <silviolucas_santos@hotmail.com>
2023-09-20 23:16:12 -0300

    feat: add AutenticacaoMiddleware

---
+++ app/Http/Middleware/AutenticacaoMiddleware.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class AutenticacaoMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        session_start();
+        if (!isset($_SESSION['email'])) return redirect()->route('site.login', ['erro' => 2]);
+        return $next($request);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ContatoController;
+use App\Http\Controllers\LoginController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PrincipalController;
 use App\Http\Controllers\SobreNosController;
@@ -17,12 +18,16 @@ use App\Http\Controllers\SobreNosController;
 */
 
 Route::get('/', [PrincipalController::class, 'principal'])->name('site.index');
+
 Route::get('/sobre-nos', [SobreNosController::class, 'sobreNos'])->name('site.sobre-nos');
+
 Route::get('/contato', [ContatoController::class, 'contato'])->name('site.contato');
 Route::post('/contato', [ContatoController::class, 'salvar'])->name('site.contato');
-Route::get('/login', function () { return 'Login'; })->name('site.login');
 
-Route::prefix('/app')->group(function() {
+Route::get('/login/{erro?}', [LoginController::class, 'index'])->name('site.login');
+Route::post('/login', [LoginController::class, 'autenticar'])->name('site.login');
+
+Route::middleware('autenticacao')->prefix('/app')->group(function() {
     Route::get('/clientes', function () { return 'Clientes'; })->name('app.clientes');
     Route::get('/fornecedores', function () { return 'Fornecedores'; })->name('app.fornecedores');
     Route::get('/produtos', function () { return 'Produtos'; })->name('app.produtos');
