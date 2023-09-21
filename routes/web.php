78db4e5b54eb0564ecc8677c3c8065422d070379
Silvio Lucas <silviolucas_santos@hotmail.com>
2023-09-20 23:49:25 -0300

    refactor: apply new controllers

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,14 @@
 <?php
 
+use App\Http\Controllers\ClienteController;
 use App\Http\Controllers\ContatoController;
+use App\Http\Controllers\FornecedorController;
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\LoginController;
-use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PrincipalController;
+use App\Http\Controllers\ProdutoController;
 use App\Http\Controllers\SobreNosController;
+use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
@@ -27,12 +31,14 @@ Route::post('/contato', [ContatoController::class, 'salvar'])->name('site.contat
 Route::get('/login/{erro?}', [LoginController::class, 'index'])->name('site.login');
 Route::post('/login', [LoginController::class, 'autenticar'])->name('site.login');
 
-Route::middleware('autenticacao')->prefix('/app')->group(function() {
-    Route::get('/clientes', function () { return 'Clientes'; })->name('app.clientes');
-    Route::get('/fornecedores', function () { return 'Fornecedores'; })->name('app.fornecedores');
-    Route::get('/produtos', function () { return 'Produtos'; })->name('app.produtos');
+Route::middleware('autenticacao')->prefix('/app')->group(function () {
+    Route::get('/home', [HomeController::class, 'index'])->name('app.home');
+    Route::get('/sair', [LoginController::class, 'sair'])->name('app.sair');
+    Route::get('/cliente', [ClienteController::class, 'index'])->name('app.cliente');
+    Route::get('/fornecedor', [FornecedorController::class, 'index'])->name('app.fornecedor');
+    Route::get('/produto', [ProdutoController::class, 'index'])->name('app.produto');
 });
 
-Route::fallback(function() {
-    echo 'A rota acessada não existe. <a href="'.route('site.index').'">Clique aqui</a> para ir para a página inicial.';
+Route::fallback(function () {
+    echo 'A rota acessada não existe. <a href="' . route('site.index') . '">Clique aqui</a> para ir para a página inicial.';
 });
