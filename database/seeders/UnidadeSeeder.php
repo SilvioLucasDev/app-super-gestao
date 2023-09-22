eb7f8ad3e506f4165ca81f9f3d771dc37c759022
Silvio Lucas <silviolucas_santos@hotmail.com>
2023-09-22 01:45:00 -0300

    feat: add files of Unidade

---
+++ database/seeders/UnidadeSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Unidade;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UnidadeSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Unidade::create([
+            'unidade' => 'UN',
+            'descricao' => 'Unidade',
+        ]);
+    }
+}
